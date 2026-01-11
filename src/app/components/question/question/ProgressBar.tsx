bb1f5cd6536a221c788c1d901ff4043144fa3090
Lionel Lebon <zenbom@gmail.com>
2026-01-11 12:06:43 +0100

    fix: display progress bar again

--- src/app/components/question/question/ProgressBar.tsx
+++ src/app/components/question/question/ProgressBar.tsx
@@ -8,7 +8,7 @@ const ProgressBar = (props: ProgressBarType): React.ReactNode => {
   const { width } = props;
   return (
     <div
-      className="h-1 w-[0%] bg-natagora transition-all duration-200 fixed z-40 top-0 left-0"
+      className="h-[2px] w-[0%] bg-natagora transition-all duration-200 fixed z-40 top-0 left-0"
       style={{ width: `${width}%` }}
       role="presentation"
     ></div>
