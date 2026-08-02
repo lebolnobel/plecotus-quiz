8301d9948f8f74fc9e91f830f1807d4fcb268a7c
Lionel Lebon <zenbom@gmail.com>
2026-08-02 20:34:41 +0200

    security: supabase & CSP

--- src/services/database.ts
+++ src/services/database.ts
@@ -31,15 +31,6 @@ export async function writeData(data: object) {
   try {
     const supabase = createClient(host, key);
 
-    const {
-      data: { user },
-      error: authError,
-    } = await supabase.auth.getUser();
-
-    if (authError || !user) {
-      return;
-    }
-
     const fingerprint = generateFingerprint();
     const { error } = await supabase
       .from('usage')
