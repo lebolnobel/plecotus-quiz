e48bc61ffa0ec54cdab067eb67c2f022c50c1d1c
Lionel Lebon <zenbom@gmail.com>
2026-08-05 17:11:14 +0200

    chore(a11y): prepare project for jsx-a11y (eslint plugin)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,19 +14,22 @@ module.exports = {
     'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended-type-checked',
     'eslint-config-prettier',
+    'plugin:jsx-a11y/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'environment.d.ts'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', '@typescript-eslint'],
+  plugins: ['react-refresh', '@typescript-eslint', 'jsx-a11y'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
     'react/no-unescaped-entities': 0,
-    'react-refresh/only-export-components': 0,
+    // Disabling accessibility rules for now, as they may require additional handling in the codebase.
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-noninteractive-tabindex': 0,
+    'jsx-a11y/no-autofocus': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/no-redundant-roles': 0,
   },
-  // other rules...
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
